5ba091d997d148265b2f259fd6009a6617526eff
Jothin-kumar <bjothinphysics@gmail.com>
2021-11-10 11:27:50 +0530

    Directory restructure

--- scripts/new_VOS_files/root/boot/boot.py
+++ scripts/new_VOS_files/root/boot/boot.py
@@ -21,4 +21,4 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-print('Hello! This OS is coming soon! visit https://github.com/Jothin-kumar/virtual-operating-system for updates!')
+print('Hello! This OS is under development! visit https://github.com/Jothin-kumar/virtual-operating-system for updates!')
